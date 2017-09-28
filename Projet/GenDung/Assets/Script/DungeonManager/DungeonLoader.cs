416e3d6f7030868b27a175afac2d21454d81edc3
sheido <af8081@gmail.com>
2017-09-28 23:39:36 +0200

    creation d'une room test d'un systeme

---
+++ Projet/GenDung/Assets/Script/DungeonManager/DungeonLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DungeonLoader : MonoBehaviour {
+
+	string 
+	activeScene;
+
+	public string map,dungeon;
+
+	public bool
+	didIInstantiate;
+
+	// Use this for initialization
+	void Start () {
+		activeScene = SceneManager.GetActiveScene ().name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (activeScene == dungeon) {
+
+		}
+	}
+
+	void changeDungeonIndex () {
+		
+	}
+
+	void LoadRoom () {
+		
+	}
+}
