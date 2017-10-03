27e2a21d1e48630d595045e8481fa524a3d33af7
sheido <af8081@gmail.com>
2017-10-03 23:23:01 +0200

    script door

---
+++ Projet/GenDung/Assets/Script/DungeonManager/Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Door {
+
+	public string doorName = "Imput a door name";
+
+	public int connectingTo; // witch room is it connecting to.
+
+	public Vector2 coordinate;
+
+	public Sprite doorSprite;
+
+}
