3ce884f5943fe0cf6233e8b830d14b85652c04ea
sheido <af8081@gmail.com>
2017-10-14 20:44:29 +0200

    clean du dongeonloader + création d'écran de fin

--- Projet/GenDung/Assets/Script/DungeonManager/DungeonLoader.cs
+++ Projet/GenDung/Assets/Script/DungeonManager/DungeonLoader.cs
@@ -9,46 +9,56 @@ using UnityEngine.EventSystems;
 
 public class DungeonLoader : MonoBehaviour {
 
-	public string activeScene, previousScene, roomType; // just a checker to see what room is the actual room that we are using.
+	public string 
+	activeScene, //check active scene
+	previousScene, //previous scene
+	roomType; // just a checker to see what room is the actual room that we are using.
 
 	public GameObject 
-	roomPrefab,
-	doorPrefab,
-	chestRoomUIPrefab,
-	fightRoomUIPrefab,
-	bossRoomUIPrefab,
-	enemyPrefabUIICON,
-	bossPrefabUIICON,
-	DebugPrefab;
+	roomPrefab,	//prefab de la room générale
+	doorPrefab,	//prefab de la porte
+	chestRoomUIPrefab,	//prefab de l'UI des salles chest
+	fightRoomUIPrefab,	//prefab de l'UI des salles fight
+	bossRoomUIPrefab,	//prefab de l'UI des salles boss
+	enemyPrefabUIICON,	//prefab de l'UI pour l'icone d'un enemi
+	bossPrefabUIICON,	//prefab de l'UI pour l'icone de boss
+	DebugPrefab,		//prefab de l'UI pour le debug ( surtout le changement de salle de donjon sur la map)
+	EndDungeonUIPrefab;	//prefab de l'UI pour l'ecran de victoire apres un donjon
 
 	GameObject 
-	BG, 
-	doorinstantiated, 
-	InformationPanel,
-	Nextdoor,
-	Previousdoor;
+	BG, //background de la salle
+	doorinstantiated; //la porte instantiée
 
-	public RoomList[] roomListDungeon; // this are the dungeons, 
+	public RoomList[] 
+	roomListDungeon; // this are the dungeons, 
 
-	public GameObject[] dungeonOnTheMap;
+	public GameObject[] 
+	dungeonOnTheMap;	//list des boutons des donjons sur la carte
 
-	int index;
-	public int dungeonIndex, dungeonUnlockedIndex;
+	int 
+	index; //index pour les salles du donjon
+	public int 
+	dungeonIndex, //index pour le donjon
+	dungeonUnlockedIndex;	//index pour le donjon unlocked
 
 	public bool
-	loadOnce3,
-	loadOnce2,
-	loadbutton,
-	loadbutton2,
-	loadOnceDoor,
-	roomIsLocked,
-	isUIinstantiated,
-	doOnceCoroutine,
-	sceneLoaded;
+	loadOnce3, //lié au godeeperintodungeon vu que c'est un bouton ca a besoin de verifier que ca ne se fait qu'une fois
+	loadOnce2,	//lié au loadRoom vu que c'est un bouton ca a besoin de verifier que ca ne se fait qu'une fois
+	loadbutton,	//pour ne charger qu'une fois la scene donjon
+	loadbutton2,	//pour ne charger qu'une fois la scene map
+	loadOnceDoor,	//pour ne charger qu'une fois la porte
+	roomIsLocked,	//permet de verouiller une porte
+	isUIinstantiated,	//verifier dans le donjon si l'interface a bien été instanciée
+	doOnceCoroutine,	//lance la coroutine qu'une fois
+	sceneLoaded,	//attendre que la scene est bien chargé
+	InstrantiateOnceEndDungeon,	//instancier une fois l'écran de fin de donjon
+	EndDungeon;	//verifier si le donjon est fini ou pas
 
 	void Start () {
+		//permet de vérifier ce qu'est la scene actuelle et d'attendre qu'elle aie fini de charger
 		SceneManager.sceneLoaded += OnSceneLoaded;
-		//editor stuff
+
+		//seulement d'en l'éditor pour charger les modules de debug
 		#if UNITY_EDITOR
 			Instantiate(DebugPrefab);
 			GameObject.Find("IncreaseUnlockIndexButton").GetComponent<Button> ().onClick.AddListener (UnlockNextDungeon);
@@ -57,22 +67,26 @@ public class DungeonLoader : MonoBehaviour {
 		#endif
 	}
 
+	//permet de vérifier ce qu'est la scene actuelle et d'attendre qu'elle aie fini de charger
 	void OnSceneLoaded(Scene scene, LoadSceneMode mode){
 		sceneLoaded = true;
 	}
 
 
 	void FixedUpdate () {
-
+		//permet de savoir quel nom de scene
 		activeScene = SceneManager.GetActiveScene ().name;
 
+		//attendre que la scene soit chargée
 		if (!sceneLoaded) {
+			
 			//-----------Dungeon gestion scene-------------//
 			if (activeScene == "Dungeon") {
 
-				//check if we did instanciate once the door
+				//initialise la référence au background de la salle
 				BG = GameObject.FindGameObjectWithTag ("backgroundOfRoom");
 
+				//permet de charger la salle room si on vient de changer de scene
 				if (previousScene != activeScene) {
 					LoadRoom ();
 				}
@@ -81,22 +95,71 @@ public class DungeonLoader : MonoBehaviour {
 			//-----------Map gestion scene-------------//
 			if (activeScene == "Map") { 
 
+				//----------ecran de fin de donjon-----------//
+				if(EndDungeon && !InstrantiateOnceEndDungeon){
 
+					//instantie l'écran de fin
+					InstrantiateOnceEndDungeon = true;
+					Instantiate(EndDungeonUIPrefab);
 
+					//verifie dans toutes les salles
+					for (int i = 0; i < roomListDungeon[dungeonIndex].RoomOfTheDungeon.Count(); i++) {
+
+						//si la salle est de type fight
+						if (roomListDungeon [dungeonIndex].RoomOfTheDungeon [i].roomType.ToString() == "fight") {
+							
+							//prendre ses enfants et instantier une icone pour chaque + definir leur parent dans l'écran de fin
+							for (int l = 0; l < roomListDungeon [dungeonIndex].RoomOfTheDungeon [i].enemies; l++) {
+
+								GameObject enemyUI;
+								enemyUI = Instantiate (enemyPrefabUIICON);
+								enemyUI.transform.SetParent (GameObject.FindGameObjectWithTag ("EnemyPanel").transform, false);
+								enemyUI.transform.GetChild(0).GetComponent<Image> ().sprite = roomListDungeon [dungeonIndex].RoomOfTheDungeon [i].enemiesList [l].enemyIcon;
+							}
+						}
+
+						//si la salle est de type boss
+						if (roomListDungeon [dungeonIndex].RoomOfTheDungeon [i].roomType.ToString() == "boss") {
+							
+							//prendre ses enfants et instantier une icone pour chaque + definir leur parent dans l'écran de fin
+							for (int l = 0; l < roomListDungeon [dungeonIndex].RoomOfTheDungeon [i].enemies; l++) {
+
+								GameObject enemyUI;
+								enemyUI = Instantiate (enemyPrefabUIICON);
+								enemyUI.transform.SetParent (GameObject.FindGameObjectWithTag ("EnemyPanel").transform, false);
+								enemyUI.transform.GetChild(0).GetComponent<Image> ().sprite = roomListDungeon [dungeonIndex].RoomOfTheDungeon [i].enemiesList [l].enemyIcon;
+							}
+
+							//vu que c'est un type boss il y a aussi le boss a instancier
+							GameObject bossUI;
+							bossUI = Instantiate (bossPrefabUIICON);
+							bossUI.transform.SetParent (GameObject.FindGameObjectWithTag ("EnemyPanel").transform, false);
+							bossUI.transform.GetChild(0).GetComponent<Image> ().sprite = roomListDungeon [dungeonIndex].RoomOfTheDungeon [i].bossList [0].bossIcon;
+						}
+					}
+				}
+
+				//va rechercher dans la liste de donjon dans le prefab de carte l'index qui permet de savoir en passant la souris dans quel donjon on va entrer
 				dungeonIndex = GameObject.FindGameObjectWithTag ("DungeonButtonMap").GetComponent<DungeonListOnMap> ().indexLocal;
+
+				//ajoute a tout les boutons sur la carte le fait de charger la salle donjon
 				dungeonOnTheMap [dungeonIndex].transform.Find("DungeonButton").GetComponent<Button> ().onClick.AddListener (LoadSceneDungeon);
 
+				//assure que les salles sont bien unlock
 				roomIsLocked = false;
+				//reinitialise le systeme de check de salle
 				previousScene = null;
 
-				print (dungeonOnTheMap.Length);
+
+				//----------Dungeon Unlocking Feature ------------//
 				if (dungeonUnlockedIndex <= dungeonOnTheMap.Length) {
-					//----------Dungeon Unlocking Feature ------------//
+					
 					for (int i = 0; i < dungeonOnTheMap.Length; i++) {
-						//---- Met faux tous les donjons non débloqué---//
+						//Met faux tous les donjons non débloqué
 						dungeonOnTheMap [i].SetActive (false);
 					}
 					for (int i = 0; i < dungeonUnlockedIndex; i++) {
+						//Met vrai tous les donjons débloqué
 						dungeonOnTheMap [i].SetActive (true);
 						dungeonOnTheMap [i].transform.Find ("DungeonButton").GetComponent<Button> ().interactable = true;
 						dungeonOnTheMap [i].transform.Find ("DungeonButton").GetComponent<Button> ().image.color = Color.white;
@@ -105,12 +168,14 @@ public class DungeonLoader : MonoBehaviour {
 						if(i +1 < dungeonOnTheMap.Length){
 							dungeonOnTheMap [i+1].SetActive (true);
 							dungeonOnTheMap [i+1].transform.Find ("DungeonButton").GetComponent<Button> ().interactable = false;
-							dungeonOnTheMap [i+1].transform.Find ("DungeonButton").GetComponent<Button> ().image.color = Color.red;
+							dungeonOnTheMap [i+1].transform.Find ("DungeonButton").GetComponent<Button> ().image.color = Color.grey;
 						}
 					}
 				}
 			}
 		} else {
+			//systeme de vérification pour voir si la scene a bien charger
+			//au sinon lance une coroutine qui attend peu et reinitialise les données
 			if (!doOnceCoroutine) {
 				doOnceCoroutine = true;
 				StartCoroutine ("WaitLoading");
@@ -122,14 +187,17 @@ public class DungeonLoader : MonoBehaviour {
 	void LoadSceneDungeon () {
 		if (!loadbutton) {
 			loadbutton = true;
+			InstrantiateOnceEndDungeon = false;
 			SceneManager.LoadScene ("Dungeon");
 		}
 	}
 
+	//charge la scene map
 	void LoadSceneMap () {
 		if (!loadbutton2) {
 			loadbutton2 = true;
 			SceneManager.LoadScene ("Map");
+			EndDungeon = true;
 		}
 	}
 
@@ -137,20 +205,20 @@ public class DungeonLoader : MonoBehaviour {
 	void LoadRoom () {
 		if (!loadOnce2) {
 			loadOnce2 = true;
-			//Debug.Log ("you have load the Room Once");
 			Instantiate (roomPrefab);
-			//index to say witch room are we using now
+
+			//reset l'index du donjon
 			index = 0;
 
+			//attribue le background de la salle
 			BG = GameObject.FindGameObjectWithTag ("backgroundOfRoom");
-
 			BG.transform.GetComponent<Image> ().sprite = roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].back;
 
 			//instantiate the door
 			loadDoor();
 
+			//permet de vérifier le type de salle
 			GetRoomType ();
-			//Debug.Log ("you have fully load the Room ");
 		}
 	}
 
@@ -158,7 +226,7 @@ public class DungeonLoader : MonoBehaviour {
 		
 	void GoDeeperInTheDungeon () {
 		if (!loadOnce3) {
-			//print ("GoDeeper index : " + index);
+			//si la salle n'est pas vérouillée
 			if (!roomIsLocked) {
 				loadOnce3 = true;
 
@@ -166,37 +234,35 @@ public class DungeonLoader : MonoBehaviour {
 				isUIinstantiated = false;
 
 				//look throught all the stats and asign them to object in the scene depending on the tags
-				//Debug.Log ("Im Index Bug B");
+				//Change le background en fonction de la salle
 				if (roomListDungeon [dungeonIndex].RoomOfTheDungeon[index].roomID <= roomListDungeon [dungeonIndex].RoomOfTheDungeon.Count)
 					BG.transform.GetComponent<Image> ().sprite = roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].back;
-			
+
+				//charge la porte
 				loadDoor ();
+
+				//permet de vérifier le type de salle
 				GetRoomType ();
 
-				//if (roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].roomID < roomListDungeon [dungeonIndex].RoomOfTheDungeon.Count ()) {	
-				//	print ("Actual Index : " + index);
+				//change l'index pour naviger dans le donjon
+				index = roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].doorList [0].connectingTo - 1;
 
-					index = roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].doorList [0].connectingTo - 1;
-				//}
+				//attend pour ne pas spammer le bouton de porte
 				StartCoroutine ("waitLagForClicking");
 				}
 			}
 		}
 
-	IEnumerator waitLagForClicking () {
-		yield return new WaitForSeconds (0.1f);
-		loadOnce3 = false;
-	}
-
+	//permet de savoir le type de la room
 	void GetRoomType()
 	{
-		//Debug.Log ("Im Index Bug C");
+		//cherche pour la salle précise et store son room type
 		if (roomListDungeon [dungeonIndex].RoomOfTheDungeon[index].roomID <= roomListDungeon [dungeonIndex].RoomOfTheDungeon.Count)
 			roomType = roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].roomType.ToString(); 
 
+		//--------CHEST---------//
 		if (roomType == "chest") {
 			roomIsLocked = true;
-			//GameObject.FindGameObjectWithTag("door").GetComponent<Button> ().onClick.AddListener (ChangeInformationPanel);
 
 			if (!isUIinstantiated) {
 				isUIinstantiated = true;
@@ -205,9 +271,9 @@ public class DungeonLoader : MonoBehaviour {
 			GameObject.FindGameObjectWithTag ("unlockRoomButton").GetComponent<Button> ().onClick.AddListener (UnlockRoom);
 		}
 
+		//--------FIGHT---------//
 		if (roomType == "fight") {
 			roomIsLocked = true;
-			//GameObject.FindGameObjectWithTag("door").GetComponent<Button> ().onClick.AddListener (ChangeInformationPanel);
 
 			if (!isUIinstantiated) {
 				isUIinstantiated = true;
@@ -215,22 +281,19 @@ public class DungeonLoader : MonoBehaviour {
 			}
 			GameObject.FindGameObjectWithTag ("unlockRoomButton").GetComponent<Button> ().onClick.AddListener (UnlockRoom);
 
+			//instantie pour chaque enemi dans la liste une icone
 			for (int i = 0; i < roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].enemies; i++) {
-
 				GameObject enemyUI;
-
 				enemyUI = Instantiate (enemyPrefabUIICON);
-
 				enemyUI.transform.SetParent (GameObject.FindGameObjectWithTag ("EnemyPanel").transform, false);
-
 				enemyUI.transform.GetChild(0).GetComponent<Image> ().sprite = roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].enemiesList [i].enemyIcon;
 			}
 
 		}
 
+		//--------BOSS---------//
 		if (roomType == "boss") {
 			roomIsLocked = true;
-			//GameObject.FindGameObjectWithTag("door").GetComponent<Button> ().onClick.AddListener (ChangeInformationPanel);
 
 			if (!isUIinstantiated) {
 				isUIinstantiated = true;
@@ -238,67 +301,48 @@ public class DungeonLoader : MonoBehaviour {
 			}
 			GameObject.FindGameObjectWithTag ("unlockRoomButton").GetComponent<Button> ().onClick.AddListener (UnlockRoom);
 
+			//instantie l'icone de boss
 			GameObject bossUI;
-
 			bossUI = Instantiate (bossPrefabUIICON);
 			bossUI.transform.SetParent (GameObject.FindGameObjectWithTag ("EnemyPanel").transform, false);
-
 			bossUI.transform.GetChild(0).GetComponent<Image> ().sprite = roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].bossList [0].bossIcon;
 
+			//instantie pour chaque enemi dans la liste une icone
 			for (int i = 0; i < roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].enemies; i++) {
 
 				GameObject enemyBossUI;
-
 				enemyBossUI = Instantiate (enemyPrefabUIICON);
 				enemyBossUI.transform.SetParent (GameObject.FindGameObjectWithTag ("EnemyPanel").transform, false);
-
 				enemyBossUI.transform.GetChild(0).GetComponent<Image> ().sprite = roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].enemiesList [i].enemyIcon;
 			}
 		}
 	}
 
-	void ChangeInformationPanel(){
-		if (roomType == "chest") {
-			//Debug.Log ("Door is locked");
-			InformationPanel.GetComponent<Text> ().text = "Door is locked, find the chest first";
-		}
-		if (roomType == "fight") {
-			//Debug.Log ("Door is locked");
-			InformationPanel.GetComponent<Text> ().text = "Door is locked, you have to fight";
-		}
-		if (roomType == "boss") {
-			//Debug.Log ("Door is locked");
-			InformationPanel.GetComponent<Text> ().text = "Door is locked, you have to fight the boss";
-		}
-	}
-
+	//charge la porte suivante et ses données
 	void loadDoor () {
 		if (!loadOnceDoor) {
 			loadOnceDoor = true;
+
 			//initialise the door at each time we call it
 			GameObject[] tempDoor;
 			tempDoor = GameObject.FindGameObjectsWithTag ("door");
 
+			//desactive les portes suivantes
 			if (tempDoor != null) {
 				for (int i = 0; i < tempDoor.Length; i++) {
 					tempDoor [i].SetActive (false);
 				}
 			}
 
+			//assigne la porte a ses coordonnées
 			doorinstantiated = Instantiate (doorPrefab, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
-
 			doorinstantiated.transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform, false);
+			doorinstantiated.transform.localPosition = new Vector3 (roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].doorList [0].coordinate.x, roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].doorList [0].coordinate.y,0);
 
-
-				doorinstantiated.transform.localPosition 
-					= new Vector3 (
-					roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].doorList [0].coordinate.x, 
-					roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].doorList [0].coordinate.y,
-					0);
-
+			//assigne les scripts que la porte a lorsqu'on clique dessus en fonction de son emplacement dans le donjon
 			if (roomListDungeon [dungeonIndex].RoomOfTheDungeon [index].doorList [0].doorType.ToString () == "LastDoor") {
 				Debug.Log ("hey im last door");
-				doorinstantiated.GetComponent<Button> ().onClick.AddListener (IndexAddingLoadMap);
+				doorinstantiated.GetComponent<Button> ().onClick.AddListener (LoadSceneMap);
 				doorinstantiated.GetComponent<Button> ().onClick.AddListener (UnlockNextDungeon);
 			} else {
 				doorinstantiated.GetComponent<Button> ().onClick.AddListener (GoDeeperInTheDungeon);
@@ -308,40 +352,9 @@ public class DungeonLoader : MonoBehaviour {
 		}
 	}
 
-	void IndexAddingLoadMap(){
-		LoadSceneMap ();
-	}
-
-	IEnumerator loadWaitRoom(){
-		yield return new WaitForSeconds (0.3f);
-		loadOnceDoor = false;
-	}
-
-	IEnumerator WaitLoading(){
-		yield return new WaitForSeconds (0.05f);
-		if (activeScene == "Dungeon") {
-			//InformationPanel = GameObject.FindGameObjectWithTag ("informationPanel");
-			previousScene = "DebugMap";
-		}
-		if (activeScene == "Map") {
-			dungeonOnTheMap = GameObject.FindGameObjectWithTag ("DungeonButtonMap").GetComponent<DungeonListOnMap> ().dungeonOnTheMapList;
-
-			previousScene = "DebugDungeon";
-
-			loadOnce3 = false;
-			loadOnce2 = false;
-			loadbutton = false;
-			isUIinstantiated = false;
-			loadbutton2 = false;
-		}
-		sceneLoaded = false;
-		doOnceCoroutine = false;
-	}
-
 	public void UnlockRoom () {
 		roomIsLocked = false;
 		GameObject.FindGameObjectWithTag ("canvasInDungeon").SetActive (false);
-		//print ("Unlock index : " + index);
 	}
 
 	public void UnlockNextDungeon(){
@@ -355,8 +368,50 @@ public class DungeonLoader : MonoBehaviour {
 			dungeonUnlockedIndex--;
 		}
 	}
-
+		
 	public void ResetUnlockDungeonIndex(){
 		dungeonUnlockedIndex = 1;
+	}
+
+	//----------------------------IENUMERATOR---------------------------------//
+
+	//attend que la salle soit bien chargée
+	IEnumerator loadWaitRoom(){
+		yield return new WaitForSeconds (0.1f);
+		loadOnceDoor = false;
+	}
+
+	//attend que la scene soit bien chargée
+	IEnumerator WaitLoading(){
+		yield return new WaitForSeconds (0.05f);
+
+		//reinitialise la scene pour charger a nouveau lors du prochain donjon le LOADROOM
+		if (activeScene == "Dungeon") {
+			previousScene = "";
+		}
+
+		if (activeScene == "Map") {
+			dungeonOnTheMap = GameObject.FindGameObjectWithTag ("DungeonButtonMap").GetComponent<DungeonListOnMap> ().dungeonOnTheMapList;
+
+			//reinitialise la scene pour charger a nouveau lors du prochain donjon le LOADROOM
+			previousScene = "";
+
+			//réinitialise les données
+			loadOnce3 = false;
+			loadOnce2 = false;
+			loadbutton = false;
+			isUIinstantiated = false;
+			loadbutton2 = false;
+		}
+
+		//relance la recherche des données dans le fixedUpdate
+		sceneLoaded = false;
+		doOnceCoroutine = false;
+	}
+
+	//coroutine qui attend pour ne pas spammer le bouton de porte
+	IEnumerator waitLagForClicking () {
+		yield return new WaitForSeconds (0.1f);
+		loadOnce3 = false;
 	}
 }
--- Projet/GenDung/Assets/Script/SwitchCanvas.cs
+++ Projet/GenDung/Assets/Script/SwitchCanvas.cs
@@ -59,12 +59,12 @@ public class SwitchCanvas : MonoBehaviour {
 				}
 
                 /* Ajoute de manière dynamique mon DungeonController à l'objet (Pas de modification de scène nécessaire ;) )  */
-                /*if (SceneManager.GetActiveScene().name == "Dungeon")
+               /* if (SceneManager.GetActiveScene().name == "Dungeon")
                 {
                     this.gameObject.AddComponent<DungeonController>();
                     this.gameObject.AddComponent<MouseController>();
-                }
-                */
+                }*/
+                
             }
 
 			if(activeScene == sceneNames [0]){
