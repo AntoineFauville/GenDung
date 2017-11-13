fd0d3a1e0e75c5a8ba8c7c1e9f65a4590043d59f
Antoine Fauville <antoine.fauville@fluffyfairygames.com>
2017-11-13 21:31:56 +0100

    carte finale + 5 dungeone

--- Projet/GenDung/Assets/Script/DungeonManager/RoomList.cs
+++ Projet/GenDung/Assets/Script/DungeonManager/RoomList.cs
@@ -8,5 +8,13 @@ public class RoomList : ScriptableObject {
 	public List<Room> RoomOfTheDungeon;
 
     public int dungeonGold = 1;
+
+    [Range(1, 4)]
+    public int somethign;
     
+    [Header("something")]
+    [Space(10)]
+    public int somethidddgn;
+
+
 }
