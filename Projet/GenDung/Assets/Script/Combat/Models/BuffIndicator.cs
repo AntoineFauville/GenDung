859afac7ab7a0cc2888c1588cd27eb6cdafd8212
Antoine Fauville <antoine.fauville@fluffyfairygames.com>
2017-12-06 13:20:35 +0100

    ajout du gestionnaire des buffs plus qu'a l'appeller quand on le veut

---
+++ Projet/GenDung/Assets/Script/Combat/Models/BuffIndicator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuffIndicator", menuName = "Buffs/BuffIndicator", order = 1)]
+public class BuffIndicator : ScriptableObject
+{
+    public string buffIndName = "None";
+    public int buffIndId;
+
+    public Color color;
+    public string textOfTheBuff = "+0PX";
+}
---
+++ Projet/GenDung/Assets/Script/Combat/Models/BuffIndicatorGestion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuffIndicatorGestion : MonoBehaviour {
+
+    public BuffIndicator[] buffIndicatorList;
+
+    public void GetBuffIndicator(int buffNumber,int localChar)
+    {
+        GameObject.Find("Character_" + localChar).transform.Find("Panel/Text").GetComponent<Text>().text = buffIndicatorList[buffNumber].textOfTheBuff;
+    }
+}
