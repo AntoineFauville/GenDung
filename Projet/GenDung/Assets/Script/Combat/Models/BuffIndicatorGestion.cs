994c28393f93db5c308484a1f03451b30963feab
Antoine Fauville <antoine.fauville@fluffyfairygames.com>
2017-12-06 20:43:44 +0100

    BuffIndicator color bug wtf

--- Projet/GenDung/Assets/Script/Combat/Models/BuffIndicatorGestion.cs
+++ Projet/GenDung/Assets/Script/Combat/Models/BuffIndicatorGestion.cs
@@ -9,6 +9,8 @@ public class BuffIndicatorGestion : MonoBehaviour {
 
     public void GetBuffIndicator(int buffNumber,int localChar)
     {
+        GameObject.Find("Character_" + localChar).transform.Find("Panel").GetComponent<Animator>().Play("AnimationTopPlayerBUFF");
         GameObject.Find("Character_" + localChar).transform.Find("Panel/Text").GetComponent<Text>().text = buffIndicatorList[buffNumber].textOfTheBuff;
+        GameObject.Find("Character_" + localChar).transform.Find("Panel/Text").GetComponent<Text>().Color = buffIndicatorList[buffNumber].color;
     }
 }
