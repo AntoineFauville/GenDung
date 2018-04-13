8c5d78c0a41ff75b3de3d57ebaffa61f90dbeaef
Antoine <antoine.fauville@fluffyfairygames.com>
2018-04-13 11:24:45 +0200

    chara creation scaled ui for character selection

---
+++ Projet/GenDung/Assets/Script/ScaleImage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleImage : MonoBehaviour {
+
+    public int size;
+
+	void Start () {
+        size = GameObject.Find("DontDestroyOnLoad").GetComponent<SavingSystem>().gameData.SavedSizeOfTheTeam;
+	}
+	
+	void Update () {
+
+        size = GameObject.Find("DontDestroyOnLoad").GetComponent<SavingSystem>().gameData.SavedSizeOfTheTeam;
+
+        if (size == 1)
+        {
+            this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(140,149);
+        }
+        else if (size == 2)
+        {
+            this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(237, 149);
+        }
+        else if (size == 3)
+        {
+            this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(335, 149);
+        }
+        else if (size == 4)
+        {
+            this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(434, 149);
+        }
+        else
+        {
+
+        }	
+	}
+}
