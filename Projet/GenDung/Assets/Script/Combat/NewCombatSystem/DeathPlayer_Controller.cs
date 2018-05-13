dd999266bcacf651584b025486c62f16a0fbcb93
sheido <af8081@gmail.com>
2018-05-13 09:55:40 +0200

    LittleFun avec la mort de coco sur l'init pour nous défouler ;)

---
+++ Projet/GenDung/Assets/Script/Combat/NewCombatSystem/DeathPlayer_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlayer_Controller : MonoBehaviour {
+
+	public GameObject[] Parts;
+
+	public void Start() {
+		for (int i = 0; i < Parts.Length; i++) {
+			Parts [i].GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Kinematic;
+
+			Parts [i].GetComponent<SpringJoint2D> ().enabled = false;
+		}
+	}
+
+	public void death(){
+		StartCoroutine (deathAnim());
+	}
+
+	IEnumerator deathAnim(){
+
+		for (int i = 0; i < Parts.Length; i++) {
+			Parts [i].GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Dynamic;
+
+			Parts [i].GetComponent<SpringJoint2D> ().enabled = true;
+		}
+
+		yield return new WaitForSeconds (5.0f);
+
+		for (int i = 0; i < Parts.Length; i++) {
+			Parts [i].GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Static;
+
+			Parts [i].GetComponent<SpringJoint2D> ().enabled = false;
+		}
+	}
+}
