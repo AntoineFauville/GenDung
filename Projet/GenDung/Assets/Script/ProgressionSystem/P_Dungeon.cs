b927ade8786056e9e65c41b344263839bb60a37e
sheido <af8081@gmail.com>
2018-05-20 12:55:01 +0200

    Progression System done

---
+++ Projet/GenDung/Assets/Script/ProgressionSystem/P_Dungeon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Dungeon {
+
+	public string name;
+
+	public int index;
+
+	public int power_difficulty = 1;
+	public int dungeon_Reward = 1;
+}
---
+++ Projet/GenDung/Assets/Script/ProgressionSystem/Player_Button_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Button_Controller : MonoBehaviour {
+
+	ProgressionSystem PS;
+
+	public P_Player p_Player;
+
+	// Use this for initialization
+	void Start () {
+		PS = GameObject.FindObjectOfType<ProgressionSystem> ();
+	}
+
+	public void UpgradePlayerPower(){
+		
+		PS.CalculateOverallPower ();
+
+		PS.UpgradePlayer (p_Player);
+	}
+}
