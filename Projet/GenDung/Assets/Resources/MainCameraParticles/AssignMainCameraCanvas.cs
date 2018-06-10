54dc8b60d551414b7ed21e49cb096c1e941a408f
sheido <af8081@gmail.com>
2018-06-10 15:05:18 +0200

    Carte review + effect main menu

---
+++ Projet/GenDung/Assets/Resources/MainCameraParticles/AssignMainCameraCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignMainCameraCanvas : MonoBehaviour {
+
+	
+	void Awake ()
+	{
+	    this.GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+	}
+	
+}
