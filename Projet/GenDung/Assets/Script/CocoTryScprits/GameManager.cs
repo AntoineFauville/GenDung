1257ed9d75b085364f79c098a4a8d38a00111290
Alfattarte <alfatta_@hotmail.com>
2018-06-29 23:22:26 +0200

    Example grille

---
+++ Projet/GenDung/Assets/Script/CocoTryScprits/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PrefabCell;
+    public int rows;
+    public int colomns;
+
+    public Vector2 offset;
+    public Vector2 PositionInitiale;
+
+    void Start()
+    {
+        Vector2 position;
+        position = PositionInitiale;
+
+        for (int y = 0; y < colomns; y++)
+        {
+            for (int x = 0; x < rows; x++)
+            {
+                GameObject CellGameObject;
+
+                Cell cell = new Cell();
+
+                CellGameObject = InstantiatePrefab(position);
+
+                CellGameObject.name = cell.ID + "_" + x + "_" + y;
+
+                position.x += offset.x;
+            }
+            position.x = PositionInitiale.x;
+            position.y += offset.y;
+        }
+    }
+
+    private GameObject InstantiatePrefab(Vector2 pos)
+    {
+        GameObject bob;
+        bob = Instantiate(PrefabCell, pos, Quaternion.Euler(0, 0, 0));
+        return bob;
+    }
+}
