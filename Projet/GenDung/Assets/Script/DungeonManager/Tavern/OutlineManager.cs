9e7853a59df146b68a839db20fc182abe7e4f788
sheido <af8081@gmail.com>
2018-07-21 22:48:16 +0200

    illu victor + outline taverne finaly.

---
+++ Projet/GenDung/Assets/Script/DungeonManager/Tavern/OutlineManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutlineManager : MonoBehaviour
+{
+    public CustomOutline CustomOutline;
+
+    private void Start()
+    {
+        ActivateOutline(false);
+    }
+
+    public void ActivateOutline(bool activate)
+    {
+        CustomOutline.glintEffect = activate;
+        if (activate)
+        {
+            CustomOutline.m_size = 1;
+        }
+        else
+        {
+            CustomOutline.m_size = 0;
+        }
+    }
+}
