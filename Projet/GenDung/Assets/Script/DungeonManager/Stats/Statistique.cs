67e7697b1bafc39b131b2c6080262d62ff1414f7
sheido <af8081@gmail.com>
2018-08-26 16:13:10 +0200

    stats implementation

---
+++ Projet/GenDung/Assets/Script/DungeonManager/Stats/Statistique.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StatModel", menuName = "Stats/StatModel", order = 1)]
+public class Statistique : ScriptableObject
+{
+    public int HealthModificator;
+    public int PhysicDamageModificator;
+    public int BloodDamageModificator;
+    public int NatureDamageModificator;
+    public int MagicDamageModificator;
+    [Space(10)]
+    public int PhysicResistanceModificator;
+    public int BloodResistanceModificator;
+    public int NatureResistanceModificator;
+    public int MagicResistanceModificator;
+    [Space(10)]
+    public int DodgingChances;
+    public int CriticalChances;
+}
