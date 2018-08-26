6c018ef65e3886bdee885a80b77ce78426784b7c
sheido <af8081@gmail.com>
2018-08-26 15:09:51 +0200

    CoolDownManager + CoolDownView

---
+++ Projet/GenDung/Assets/Script/Combat/CoolDownManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolDownManager : MonoBehaviour
+{
+    [SerializeField] public CoolDownView[] CoolDownManagers;
+
+    public void UpdateCoolDownViews(int coolDownViewToUpdate, int amountOfTotalTurn, int amountOfturnLeft)
+    {
+        CoolDownManagers[coolDownViewToUpdate].UpdateCoolDownView(true, amountOfTotalTurn, amountOfturnLeft);
+    }
+
+    public void ResetCoolDownViews(int coolDownViewToUpdate, int turnAmount)
+    {
+        CoolDownManagers[coolDownViewToUpdate].ResetCoolDownView(false);
+    }
+}
