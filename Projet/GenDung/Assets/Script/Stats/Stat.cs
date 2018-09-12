20e1ddf20640969f7b1fd850dd94e888bc8e7d07
sheido <af8081@gmail.com>
2018-09-12 21:40:52 +0200

    Stat partie 1

---
+++ Projet/GenDung/Assets/Script/Stats/Stat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stat", menuName = "Characters/Stat", order = 1)]
+public class Stat : ScriptableObject
+{
+    [Space(10)]
+    //health
+    public int HealthModificator;
+    [Space(10)]
+    //Damage
+    public int PhysicModificator;
+    public int BloodModificator;
+    public int NatureModificator;
+    public int ArcaneModificator;
+    [Space(10)]
+    //resistance
+    public int PhysicalResistance;
+    public int BloodResistance;
+    public int NatureResistance;
+    public int ArcaneResistance;
+    [Space(10)]
+    //Chances
+    public int DodgingChance;
+    public int CriticalChance;
+    
+}
---
+++ Projet/GenDung/Assets/Script/Stats/StatAccessor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatAccessor : MonoBehaviour
+{
+    public Stat Strength;
+    public Stat Knowledge;
+    public Stat Dexterity;
+    public Stat Wisdom;
+    public Stat Survivability;
+
+}
